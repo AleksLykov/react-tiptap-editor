0c7633d58acb959e95b8f6295e7fca13f04814ed
Aleksandr <alekslykov@gmail.com>
2026-03-15 14:55:23 +0400

    divide and fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Editor } from "./editor";
 
+import "./app.scss";
+
 function App() {
   const content = `<p><strong>Welcome to the React Tiptap Editor!</strong></p><p>This is a example of a rich text editor built with Tiptap. You can use the toolbar above to format your text, insert images, and more.</p><p>Feel free to explore the features and customize it to your needs!</p>`;
 
